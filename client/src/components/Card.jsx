38654cb484d51151f20ca052ab0452c0cb5952fa
KimberleyEvans-Parker <kim@evans-parker.com>
2020-03-10 22:09:27 +1300

    media colours and icons

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+/* colours and logos: 
+ * reddit:  #FF4500 https://redditupvoted.files.wordpress.com/2015/10/reddit_icon_twitter_fb.png
+ * spotify: #23D05F https://pbs.twimg.com/profile_images/558366562424332288/8ObpK74F.png
+ * github:  #010101 https://avatars0.githubusercontent.com/u/9919?s=280&v=4
+ * twitter: #05ACF0 https://pmcdeadline2.files.wordpress.com/2016/09/twitter-logo.jpg
+ * /
+
 /* available props: 
  * text: title, username, date_time, text, media
  * image link: user_icon, image
