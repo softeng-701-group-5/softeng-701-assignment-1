13d3d27fa780bc33546c45b108b979e313bdbb72
KimberleyEvans-Parker <kim@evans-parker.com>
2020-03-10 21:48:16 +1300

    image via link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
       user_icon="" 
       date_time="17/05/19" 
       text="This is a post from John" 
-      image="<img src='pic_trulli.jpg' alt='Italian Trulli'>" 
+      image_link="http://2.bp.blogspot.com/-8ms4E7aH4w8/UMztXOsniqI/AAAAAAAAIW0/qql0GcspDzs/s1600/2298.jpg"
       // image="" 
       media="Twitter" />
     </div>;
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-// available props: title, username, user_icon, date_time, text, image, media
+/* available props: 
+ * text: title, username, date_time, text, media
+ * image link: user_icon, image
+ */
 function Card(props) {
 
     return (
         <center>
-        <div style = {{width: "400px", background: "grey", padding:"10px", textAlign:"left"}}>
+        <div style = {{width: "400px", background: "#e6e6e6", padding:"10px", textAlign:"left"}}>
             <h1>{props.title}</h1>
             <h2>{props.username}</h2>
             <p>{props.text}</p>
 
             {/* only displays if there is an image */}
-            <div>{props.image ? props.image : null}</div>
+            <div>{props.image_link ? <img src={props.image_link} style = {{width: "400px"}}></img> : null}</div>
         </div>
         </center>
     );
