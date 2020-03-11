3faf7155c43fa6c4cae26bf1ce8616ee97e94157
KimberleyEvans-Parker <kim@evans-parker.com>
2020-03-11 13:04:59 +1300

    positioned title and subtitle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,13 @@ import Card from './components/Card'
 export const App = () => {
   return <div>
     Welcome to Feeder client.
-    <Card title="Card 1" 
-      username="John Smith" 
-      user_icon="" 
+    <Card title="John updated his profile" 
+      username="John Smith"
+      user_icon="http://2.bp.blogspot.com/-8ms4E7aH4w8/UMztXOsniqI/AAAAAAAAIW0/qql0GcspDzs/s1600/2298.jpg" 
       date_time="17/05/19" 
-      text="This is a post from John" 
+      text="This is my new profile picture! Hope you like it :)" 
       image_link="http://2.bp.blogspot.com/-8ms4E7aH4w8/UMztXOsniqI/AAAAAAAAIW0/qql0GcspDzs/s1600/2298.jpg"
-      // image="" 
-      media="Twitter" />
-    </div>;
+      media="twitter" 
+    />
+  </div>;
 };
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -24,29 +24,56 @@ const useStyles = makeStyles(theme => ({
  * spotify: #23D05F https://pbs.twimg.com/profile_images/558366562424332288/8ObpK74F.png
  * github:  #010101 https://avatars0.githubusercontent.com/u/9919?s=280&v=4
  * twitter: #05ACF0 https://pmcdeadline2.files.wordpress.com/2016/09/twitter-logo.jpg
- * /
+ */
+
+const barcolour=(param)=>{
+  switch(param) {
+    case "reddit":
+      return "#FF4500"
+    case "spotify":
+      return "#23D05F"
+    case "github":
+      return "#010101"
+    case "twitter":
+      return "#05ACF0"
+    default:
+      return "#b3b3b3"
+  }
+}
 
 /* available props: 
  * text: title, username, date_time, text, media
  * image link: user_icon, image
  */
 function Card(props) {
-    
+
   const classes = useStyles();
 
     return (
         <center>
-        <div style = {{width: "400px", background: "#e6e6e6", padding:"10px", textAlign:"left"}}>
-            <div className={classes.root}>
-                <Avatar alt="Remy Sharp" src="https://img.icons8.com/windows/64/000000/user.png" style={{background: "grey"}} className={classes.large} />
-            </div>
-            <h1>{props.title}</h1>
-            <h2>{props.username}</h2>
+        <div style = {{width: "500px", background: "#f2f2f2", padding:"10px", textAlign:"left", boxShadow:"5px 5px 5px grey", borderRadius: "8px"}}>
+            {props.user_icon ? <Avatar alt="Remy Sharp" src={props.user_icon} className={classes.large} style={{float: "left", margin:"10px"}} /> : <Avatar alt="Remy Sharp" src="https://img.icons8.com/windows/64/000000/user.png" style={{background: "#b3b3b3", float: "left", margin:"10px"}} className={classes.large} />}
+            <h1 style={{lineHeight: "10px"}}>{props.title}</h1>
+            <h2 style={{lineHeight: "10px", fontSize: "20px", color: "grey"}}>{props.username}</h2>
             <p>{props.text}</p>
 
             {/* only displays if there is an image */}
-            <div>{props.image_link ? <img src={props.image_link} style = {{width: "400px"}}></img> : null}</div>
+            <div>{props.image_link ? <img src={props.image_link} style = {{width: "500px"}}></img> : null}</div>
         </div>
+
+        {/* {switch({props.media}) {
+          case "reddit":
+            <p>some text</p>
+            // <div style = {{width: "400px", background: "#FF4500"}, padding:"10px", textAlign:"left"}>
+            //   <img src="https://redditupvoted.files.wordpress.com/2015/10/reddit_icon_twitter_fb.png" style = {{width: "40px"}}></img>
+            // </div>
+            break;
+        
+          default:
+            break;
+        }} */}
+        {/* barcolour({props.media}) */}
+        {/* <div style = {{width: "500px", background: "red", padding:"10px", textAlign:"left", boxShadow:"5px 5px 5px grey", borderRadius: "0px, 0px, 8px, 8px"}}></div> */}
         </center>
     );
 }
