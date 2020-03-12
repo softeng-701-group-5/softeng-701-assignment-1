f66342bb01e7f2b6b23185f244acdf6ea8654717
DeshmukhChinmay <chinmay2207@gmail.com>
2020-03-13 12:02:41 +1300

    Added the extra github properties related field in the ApplicationProperties Class. Constructor takes in extra parameters for github. Added relevant extra parameters in the RedditClientConfigTest to meet with the new number of parameters

--- server/src/main/java/com/feeder/server/ApplicationProperties.java
+++ server/src/main/java/com/feeder/server/ApplicationProperties.java
@@ -11,17 +11,26 @@ public class ApplicationProperties {
   private String redditPassword;
   private String redditClientId;
   private String redditClientSecret;
+  private String githubUsername;
+  private String githubPassword;
+  private String githubEtag;
 
   @Autowired
   public ApplicationProperties(
       @Value("${providers.reddit.username}") String redditUsername,
       @Value("${providers.reddit.password}") String redditPassword,
       @Value("${providers.reddit.clientId}") String redditClientId,
-      @Value("${providers.reddit.clientSecret}") String redditClientSecret) {
+      @Value("${providers.reddit.clientSecret}") String redditClientSecret,
+      @Value("${providers.github.username}") String githubUsername,
+      @Value("${providers.github.password}") String githubPassword,
+      @Value("${providers.github.etag}") String githubEtag) {
     this.redditUsername = redditUsername;
     this.redditPassword = redditPassword;
     this.redditClientId = redditClientId;
     this.redditClientSecret = redditClientSecret;
+    this.githubUsername = githubUsername;
+    this.githubPassword = githubPassword;
+    this.githubEtag = githubEtag;
   }
 
   public String getRedditUsername() {
@@ -39,4 +48,17 @@ public class ApplicationProperties {
   public String getRedditClientSecret() {
     return redditClientSecret;
   }
+
+  public String getGithubUsername() {
+    return githubUsername;
+  }
+
+  public String getGithubPassword() {
+    return githubPassword;
+  }
+
+  public String getGithubEtag() {
+    return githubEtag;
+  }
+
 }
--- server/src/main/java/com/feeder/server/provider/github/GithubFeedProvider.java
+++ server/src/main/java/com/feeder/server/provider/github/GithubFeedProvider.java
@@ -2,15 +2,24 @@ package com.feeder.server.provider.github;
 
 import com.feeder.server.model.GithubData;
 import com.feeder.server.provider.FeedProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 @Service
 public class GithubFeedProvider implements FeedProvider<GithubData> {
+  
+
+  @Autowired
+  public GithubFeedProvider() {
+
+  }
 
   @Override
   public Flux<GithubData> getFeed() {
     // TODO: Github team to implement
+
     return Flux.empty();
   }
 }
