c788338f6776f081c4966853eaa0ec632aa831f7
DeshmukhChinmay <chinmay2207@gmail.com>
2020-03-13 21:42:44 +1300

    Added web client to get the response from the github api

--- server/src/main/java/com/feeder/server/provider/github/GithubFeedProvider.java
+++ server/src/main/java/com/feeder/server/provider/github/GithubFeedProvider.java
@@ -3,11 +3,15 @@ package com.feeder.server.provider.github;
 import com.feeder.server.ApplicationProperties;
 import com.feeder.server.model.GithubData;
 import com.feeder.server.provider.FeedProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -17,13 +21,35 @@ public class GithubFeedProvider implements FeedProvider<GithubData> {
   private final String GITHUB_API_BASE_URL = "https://api.github.com";
   private final String GITHUB_v3_MIME_TYPE = "application/vnd.github.v3+json";
   private String eTag = "";
+  private static final Logger logger = LoggerFactory.getLogger(GithubFeedProvider.class);
 
   @Autowired
   ApplicationProperties applicationProperties;
 
   private WebClient.Builder webClientBuilder;
 
-  public WebClient.Builder getWebClientBuilder() {
+  @Override
+  public Flux<GithubData> getFeed() {
+
+    String apiEndpointReceivedEvents = "/users/" + applicationProperties.getGithubUsername() + "/received_events";
+
+    WebClient webClient = getWebClientBuilder().build();
+
+    return webClient.get()
+            .uri(apiEndpointReceivedEvents)
+            .exchange()
+            .doOnSuccess(clientResponse -> assignEtag(clientResponse.headers().header("Etag")))
+            .flatMapMany(clientResponse -> clientResponse.bodyToFlux(GithubData.class));
+
+  }
+
+  private void assignEtag(List<String> etag) {
+    for (String s: etag) {
+      this.eTag = s;
+    }
+  }
+
+  private WebClient.Builder getWebClientBuilder() {
     if (this.webClientBuilder == null) {
       this.webClientBuilder = WebClient.builder()
               .baseUrl(GITHUB_API_BASE_URL)
@@ -35,23 +61,4 @@ public class GithubFeedProvider implements FeedProvider<GithubData> {
     return this.webClientBuilder;
   }
 
-  private void assignEtag(List<String> etag) {
-    for (String s: etag) {
-      this.eTag = s;
-    }
-  }
-
-  @Override
-  public Flux<GithubData> getFeed() {
-    // TODO: Github team to implement
-
-
-    WebClient webClient = getWebClientBuilder().build();
-
-    return webClient.get()
-            .uri("/users//received_events")
-            .exchange()
-            .doOnSuccess(clientResponse -> assignEtag(clientResponse.headers().header("Etag")))
-            .flatMapMany(clientResponse -> clientResponse.bodyToFlux(GithubData.class));
-  }
 }
