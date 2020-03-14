3d6459e8fe5c00d0c0fa4fd0e3b4c3cc6ab0b9b4
Tony Liu <ipwnu777@gmail.com>
2020-03-15 12:47:30 +1300

    Moved blocking call to elastic threadpool and cache the response (to handle rate limiting)
    Benchmark:
    Blocking: 3.45 seconds
    Non-blocking 0.006 seconds

--- server/src/main/java/com/feeder/server/provider/twitter/TwitterFeedProvider.java
+++ server/src/main/java/com/feeder/server/provider/twitter/TwitterFeedProvider.java
@@ -2,42 +2,62 @@ package com.feeder.server.provider.twitter;
 
 import com.feeder.server.model.TwitterData;
 import com.feeder.server.provider.FeedProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class TwitterFeedProvider implements FeedProvider<TwitterData> {
 
+  Logger logger = LoggerFactory.getLogger(TwitterFeedProvider.class);
   @Autowired private Twitter twitter;
+  private ResponseList<Status> lastResponse;
 
   @Override
   public Flux<TwitterData> getFeed() {
-    List<Status> statuses;
-    List<TwitterData> modelledResponse = new ArrayList<>();
+
+    long start = System.nanoTime();
 
     try {
-      statuses = twitter.getHomeTimeline();
-
-      for (Status status : statuses) {
-        modelledResponse.add(
-                TwitterData.newBuilder()
-                .tweet(status.getText())
-                .profileUsername(status.getUser().getName())
-                .profileImageURI(status.getUser().get400x400ProfileImageURL())
-                .tweetPostDate(status.getCreatedAt())
-                .build()
-        );
-      }
-      return Flux.just(modelledResponse).flatMapSequential(Flux::fromIterable);
-    } catch (TwitterException e) {
-      e.printStackTrace();
-      return Flux.error(new RuntimeException("An error occurred retrieving Tweets"));
+      return Mono.fromCallable(() -> twitter.getHomeTimeline())
+          .subscribeOn(Schedulers.elastic())
+          .doOnSuccess(
+              statuses -> {
+                // Cache the statuses
+                if (statuses != null) {
+                  lastResponse = statuses;
+                }
+              })
+          .onErrorResume(
+              // Fallback to cached response when rate limited
+              TwitterException.class,
+              e -> {
+                logger.info(e.getMessage());
+                if (lastResponse != null) {
+                  return Mono.just(lastResponse);
+                }
+                return Mono.empty();
+              })
+          .flatMapMany(Flux::fromIterable)
+          .map(
+              status ->
+                  TwitterData.newBuilder()
+                      .tweet(status.getText())
+                      .profileUsername(status.getUser().getName())
+                      .profileImageURI(status.getUser().get400x400ProfileImageURL())
+                      .tweetPostDate(status.getCreatedAt())
+                      .build());
+    } finally{
+      long stop = System.nanoTime();
+      logger.info("Time taken: " + (stop - start) + "ns");
     }
   }
 }
