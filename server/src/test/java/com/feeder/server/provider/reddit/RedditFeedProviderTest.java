c93826a7a7b41b9730012b8e91918f7916ea99e3
Harrison Leach <HarrisonL7190@gmail.com>
2020-03-15 09:51:28 +1300

    Update RedditFeedProviderTest to work with Flux
    - submission needed to be mocked out

--- server/src/test/java/com/feeder/server/provider/reddit/RedditFeedProviderTest.java
+++ server/src/test/java/com/feeder/server/provider/reddit/RedditFeedProviderTest.java
@@ -1,50 +1,71 @@
 package com.feeder.server.provider.reddit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.List;
+import com.feeder.server.model.RedditData;
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Submission;
 import net.dean.jraw.models.SubredditSort;
 import net.dean.jraw.pagination.DefaultPaginator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Flux;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @WebFluxTest(controllers = RedditFeedProvider.class)
 @ExtendWith(MockitoExtension.class)
 public class RedditFeedProviderTest {
 
   @MockBean private RedditClient mockRedditClient;
-
+  @Mock private Submission mockSubmission;
   @Autowired private RedditFeedProvider subject;
+
+  @BeforeEach
+  public void setUp() {
+    when(mockSubmission.getTitle()).thenReturn("test");
+    when(mockSubmission.getAuthor()).thenReturn("test");
+    when(mockSubmission.getCreated()).thenReturn(Date.from(Instant.EPOCH));
+    when(mockSubmission.getSubreddit()).thenReturn("test");
+    when(mockSubmission.getUrl()).thenReturn("test");
+    when(mockSubmission.getThumbnail()).thenReturn("test");
+    when(mockSubmission.getSelfText()).thenReturn("test");
+  }
 
   @Test
   public void testGetFeed() {
     // arrange
-    int expectedNumberOfListings = 2;
+    int expectedNumberOfSubmissions = 1;
     DefaultPaginator<Submission> mockPaginator = mock(DefaultPaginator.class);
-    List<Listing<Submission>> mockList = Arrays.asList(new Listing[expectedNumberOfListings]);
     DefaultPaginator.Builder<Submission, SubredditSort> builder =
         mock(DefaultPaginator.Builder.class);
 
+    Iterator<Listing<Submission>> mockIterator = spy(Iterator.class);
+    Submission[] submissionsArray = new Submission[] {mockSubmission};
+    List<Submission> submissions = Arrays.asList(submissionsArray);
+    Listing<Submission> mockListings = Listing.create("nextName", submissions);
+
     when(mockRedditClient.frontPage()).thenReturn(builder);
-    when(builder.limit(anyInt())).thenReturn(builder);
     when(builder.build()).thenReturn(mockPaginator);
-    when(mockPaginator.accumulate(anyInt())).thenReturn(mockList);
+    when(mockPaginator.iterator()).thenReturn(mockIterator);
+    when(mockIterator.next()).thenReturn(mockListings);
 
     // act
-    List<Listing<Submission>> result = subject.getFeed(expectedNumberOfListings);
+    Flux<RedditData> result = subject.getFeed();
 
     // assert
-    assertEquals(result.size(), expectedNumberOfListings);
+    assertEquals(expectedNumberOfSubmissions, result.collectList().block().size());
   }
 }
