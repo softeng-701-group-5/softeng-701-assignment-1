db3db1ca9ac01956e9bc0f87bfc3d005dcf10ca5
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-03-15 10:27:47 +1300

    Refactored MediaCard component

--- client/src/components/MediaCard.jsx
+++ client/src/components/MediaCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Avatar,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 
 import TwitterIcon from './../assets/twitter-icon.svg';
 import SpotifyIcon from './../assets/spotify-icon.svg';
@@ -28,16 +28,6 @@ const useStyles = makeStyles(theme => ({
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
   },
   avatar: {
     backgroundColor: '#d9d9d9',
@@ -90,34 +80,31 @@ export const MediaCard = props => {
           <Avatar
             alt={props.username}
             src={
-              props.avatarLink
-                ? props.avatarLink
-                : 'https://img.icons8.com/windows/64/000000/user.png'
+              props.avatarLink ||
+              'https://img.icons8.com/windows/64/000000/user.png'
             }
             className={classes.avatar}
           />
         }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
         title={props.title}
-        subheader={props.username + ' - ' + props.relativeTime}
+        subheader={`${props.username} - ${props.relativeTime}`}
       />
-      {props.imageLink ? (
+
+      {props.imageLink && (
         <CardMedia className={classes.media} image={props.imageLink} />
-      ) : null}
+      )}
+
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {props.mainText}
         </Typography>
       </CardContent>
+
       <CardActions
         style={{ backgroundColor: barColour(props.media) }}
         disableSpacing
       >
-        <img src={mediaIcon(props.media)} />
+        <img src={mediaIcon(props.media)} alt={`${props.media} logo`} />
       </CardActions>
     </Card>
   );
