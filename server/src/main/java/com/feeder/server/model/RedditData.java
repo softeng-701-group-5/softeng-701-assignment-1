a5dc5df44c794cdc4d2b36d860a8f9544977e23d
Shunji Takano <stak469@aucklanduni.ac.nz>
2020-03-14 23:04:54 +1300

    Add functionality to getFeed() and RedditData.

--- server/src/main/java/com/feeder/server/model/RedditData.java
+++ server/src/main/java/com/feeder/server/model/RedditData.java
@@ -1,3 +1,77 @@
 package com.feeder.server.model;
 
-public abstract class RedditData extends GenericData {}
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.google.auto.value.AutoValue;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
+
+@AutoValue
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
+@JsonDeserialize(builder = AutoValue_RedditData.Builder.class)
+@Repository
+public abstract class RedditData extends GenericData {
+
+    public static Builder newBuilder() {return new AutoValue_RedditData.Builder();}
+
+    @Override
+    public Type feedType() {
+        return Type.REDDIT;
+    }
+
+    /** This property is required for serialization. This means all posts must contain a title. */
+    @JsonProperty("title")
+    public abstract String title();
+
+    @JsonProperty("author")
+    public abstract String author();
+
+    @JsonProperty("created")
+    public abstract Date created();
+
+    @JsonProperty("subreddit")
+    public abstract String subreddit();
+
+    @JsonProperty("url")
+    public abstract String url();
+
+
+    /**
+     * {@link java.util.Optional} means this property is NOT required for serialization. Perhaps not
+     * all posts contain images. If there is no thumbnail, then the string "self" is returned.
+     */
+    @JsonProperty("thumnail")
+    public abstract Optional<String> thumnail();
+
+    /**
+     * Self Text is the text within a post. Not all posts on reddit may contain text. If there is no text, then
+     * an empty string is returned.
+     */
+    @JsonProperty("selftext")
+    public abstract Optional<String> selftext();
+
+
+    @AutoValue.Builder
+    @JsonPOJOBuilder(withPrefix = "")
+    public interface Builder {
+        RedditData.Builder title(String title);
+
+        RedditData.Builder author(String author);
+
+        RedditData.Builder created(Date created);
+
+        RedditData.Builder subreddit(String subreddit);
+
+        RedditData.Builder url(String url);
+
+        RedditData.Builder thumnail(String thumnail);
+
+        RedditData.Builder selftext(String selftext);
+
+        RedditData build();
+    }
+}
--- server/src/main/java/com/feeder/server/provider/reddit/RedditFeedProvider.java
+++ server/src/main/java/com/feeder/server/provider/reddit/RedditFeedProvider.java
@@ -2,7 +2,6 @@ package com.feeder.server.provider.reddit;
 
 import com.feeder.server.model.RedditData;
 import com.feeder.server.provider.FeedProvider;
-import java.util.List;
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Submission;
@@ -10,6 +9,8 @@ import net.dean.jraw.pagination.Paginator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.util.Iterator;
 
 /**
  * A RedditFeedProvider is responsible for retrieving Reddit posts from a users homepage from the
@@ -20,28 +21,28 @@ public class RedditFeedProvider implements FeedProvider<RedditData> {
 
   @Autowired private RedditClient redditClient;
 
-  /**
-   * Returns Reddit posts/submissions on the users front page, the user is defined by the
-   * ApplicationProperties.
-   *
-   * @param numberOfItems number of submissions to return
-   * @return List of submissions
-   */
-  public List<Listing<Submission>> getFeed(int numberOfItems) {
-
-    // Make a request for the front page
-    Paginator paginator = redditClient.frontPage().limit(numberOfItems).build();
-
-    // Only get the first page of results, because pagination is not yet supported
-    List<Listing<Submission>> submissions = paginator.accumulate(1);
-
-    // TODO: Map the submissions to a different type for processing with other social media feeds
-    return submissions;
-  }
-
   @Override
   public Flux<RedditData> getFeed() {
-    // TODO: Reddit team to implement
-    return Flux.empty();
+    Paginator paginator = redditClient.frontPage().build();
+
+    // Only get the first page of results, because pagination is not yet supported
+    Iterator<Listing<Submission>> items = paginator.iterator();
+
+    // Iterate through all the posts in the front page and build the RedditDate type for each post.
+    Flux<RedditData> redditDataFlux =
+            Flux.fromIterable(items.next())
+                    .map(submission ->
+                            RedditData.newBuilder()
+                                    .title(submission.getTitle())
+                                    .author(submission.getAuthor())
+                                    .created(submission.getCreated())
+                                    .subreddit(submission.getSubreddit())
+                                    .url(submission.getUrl())
+                                    .thumnail(submission.getThumbnail())
+                                    .selftext(submission.getSelfText())
+                                    .build()
+                    );
+
+    return redditDataFlux;
   }
 }
