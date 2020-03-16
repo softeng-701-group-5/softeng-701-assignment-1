b14f1848e7f304b6fdb3b64f516544e1940e43d9
Tony Liu <ipwnu777@gmail.com>
2020-03-16 14:10:04 +1300

    Simplify HackerNewsFeedProvider (#83)

--- server/src/main/java/com/feeder/server/provider/hackernews/HackerNewsFeedProvider.java
+++ server/src/main/java/com/feeder/server/provider/hackernews/HackerNewsFeedProvider.java
@@ -8,37 +8,22 @@ import reactor.core.publisher.Flux;
 
 @Service
 public class HackerNewsFeedProvider implements FeedProvider<HackerNewsData> {
+
+  private WebClient client = WebClient.create();
+
   @Override
   public Flux<HackerNewsData> getFeed() {
-    WebClient client = WebClient.create();
-    Flux<HackerNewsData> feed =
-        client
-            .get()
-            .uri("https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty")
-            .retrieve()
-            .bodyToFlux(Integer.class)
-            .flatMap(
-                id ->
-                    client
-                        .get()
-                        .uri("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
-                        .retrieve()
-                        .bodyToFlux(HackerNewsData.class)
-                        .map(
-                            data ->
-                                HackerNewsData.newBuilder()
-                                    .title(data.title().get())
-                                    .by(data.by().get())
-                                    .time(data.time().get())
-                                    .url(
-                                        data.url()
-                                            .orElse(
-                                                "https://news.ycombinator.com/ask/"
-                                                    + id.toString()))
-                                    .text(data.text().orElse(""))
-                                    .score(data.score().get())
-                                    .build()));
-
-    return feed;
+    return client
+        .get()
+        .uri("https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty")
+        .retrieve()
+        .bodyToFlux(Integer.class)
+        .flatMap(
+            id ->
+                client
+                    .get()
+                    .uri("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
+                    .retrieve()
+                    .bodyToFlux(HackerNewsData.class));
   }
 }
