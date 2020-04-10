2fd045ebc374f7314f5ebdbda0611058ace7aa60
Casey Wong <cwong367@gmail.com>
2020-04-10 15:50:47 +1200

    Change indentations in line with style guide

--- server/src/main/java/com/feeder/server/model/CovidNineteenData.java
+++ server/src/main/java/com/feeder/server/model/CovidNineteenData.java
@@ -11,66 +11,65 @@ import com.google.auto.value.AutoValue;
 @JsonInclude(JsonInclude.Include.NON_ABSENT)
 @JsonDeserialize(builder = AutoValue_CovidNineteenData.Builder.class)
 public abstract class CovidNineteenData extends GenericData {
+  public static Builder newBuilder() { return new AutoValue_CovidNineteenData.Builder(); }
 
-    public static Builder newBuilder() { return new AutoValue_CovidNineteenData.Builder(); }
+  @Override
+  public Type feedType() { return Type.COVIDNINETEEN; }
 
-    @Override
-    public Type feedType() { return Type.COVIDNINETEEN; }
+  /** These properties are required for serialization, all posts must contain them. */
+  @JsonProperty("totalConfirmed")
+  public abstract String totalConfirmed();
 
-    /** These properties are required for serialization, all posts must contain them. */
-    @JsonProperty("totalConfirmed")
-    public abstract String totalConfirmed();
+  @JsonProperty("totalDeaths")
+  public abstract String totalDeaths();
 
-    @JsonProperty("totalDeaths")
-    public abstract String totalDeaths();
+  @JsonProperty("totalProbable")
+  public abstract String totalProbable();
 
-    @JsonProperty("totalProbable")
-    public abstract String totalProbable();
+  @JsonProperty("totalRecovered")
+  public abstract String totalRecovered();
 
-    @JsonProperty("totalRecovered")
-    public abstract String totalRecovered();
+  @JsonProperty("totalHospitalised")
+  public abstract String totalHospitalised();
 
-    @JsonProperty("totalHospitalised")
-    public abstract String totalHospitalised();
+  @JsonProperty("newConfirmed")
+  public abstract String newConfirmed();
 
-    @JsonProperty("newConfirmed")
-    public abstract String newConfirmed();
+  @JsonProperty("newDeaths")
+  public abstract String newDeaths();
 
-    @JsonProperty("newDeaths")
-    public abstract String newDeaths();
+  @JsonProperty("newProbable")
+  public abstract String newProbable();
 
-    @JsonProperty("newProbable")
-    public abstract String newProbable();
+  @JsonProperty("newRecovered")
+  public abstract String newRecovered();
 
-    @JsonProperty("newRecovered")
-    public abstract String newRecovered();
+  @JsonProperty("newHospitalised")
+  public abstract String newHospitalised();
 
-    @JsonProperty("newHospitalised")
-    public abstract String newHospitalised();
+  @AutoValue.Builder
+  @JsonPOJOBuilder(withPrefix = "")
+  public interface Builder {
+    CovidNineteenData.Builder totalConfirmed(String totalConfirmed);
 
-    @AutoValue.Builder
-    @JsonPOJOBuilder(withPrefix = "")
-    public interface Builder {
-        CovidNineteenData.Builder totalConfirmed(String totalConfirmed);
+    CovidNineteenData.Builder totalDeaths(String totalDeaths);
 
-        CovidNineteenData.Builder totalDeaths(String totalDeaths);
+    CovidNineteenData.Builder totalProbable(String totalProbable);
 
-        CovidNineteenData.Builder totalProbable(String totalProbable);
+    CovidNineteenData.Builder totalRecovered(String totalRecovered);
 
-        CovidNineteenData.Builder totalRecovered(String totalRecovered);
+    CovidNineteenData.Builder totalHospitalised(String totalHospitalised);
 
-        CovidNineteenData.Builder totalHospitalised(String totalHospitalised);
+    CovidNineteenData.Builder newConfirmed(String newConfirmed);
 
-        CovidNineteenData.Builder newConfirmed(String newConfirmed);
+    CovidNineteenData.Builder newDeaths(String newDeaths);
 
-        CovidNineteenData.Builder newDeaths(String newDeaths);
+    CovidNineteenData.Builder newProbable(String newProbable);
 
-        CovidNineteenData.Builder newProbable(String newProbable);
+    CovidNineteenData.Builder newRecovered(String newRecovered);
 
-        CovidNineteenData.Builder newRecovered(String newRecovered);
+    CovidNineteenData.Builder newHospitalised(String newHospitalised);
 
-        CovidNineteenData.Builder newHospitalised(String newHospitalised);
-
-        CovidNineteenData build();
-    }
+    CovidNineteenData build();
+  }
 }
