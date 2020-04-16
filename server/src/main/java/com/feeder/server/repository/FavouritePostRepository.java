518442369eda12d594e7d7b9d4b7c35747ed996b
tnzhu <tzhu442@aucklanduni.ac.nz>
2020-04-16 18:16:09 +1200

    Added endpoints for favourite posts

--- server/src/main/java/com/feeder/server/repository/FavouritePostRepository.java
+++ server/src/main/java/com/feeder/server/repository/FavouritePostRepository.java
@@ -4,7 +4,9 @@ import com.feeder.server.model.FavouritePost;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FavouritePostRepository extends MongoRepository<FavouritePost, String> {
-
+    List<FavouritePost> findByUser(String uid);
 }
--- server/src/main/java/com/feeder/server/service/FavouritePostService.java
+++ server/src/main/java/com/feeder/server/service/FavouritePostService.java
@@ -1,17 +1,27 @@
 package com.feeder.server.service;
 
+import com.feeder.server.model.FavouritePost;
 import com.feeder.server.repository.FavouritePostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FavouritePostService {
     @Autowired
     FavouritePostRepository favouritePostRepository;
 
-    //Get
+    public List<FavouritePost> getFavouritePosts(String uid) {
+        return favouritePostRepository.findByUser(uid);
+    }
 
-    //Delete
+    public void removePostFromFavourites(String id) {
+        favouritePostRepository.deleteById(id);
+    }
 
+    public FavouritePost saveFavouritePost(FavouritePost favouritePost){
+        favouritePostRepository.save(favouritePost);
+    }
 
 }
