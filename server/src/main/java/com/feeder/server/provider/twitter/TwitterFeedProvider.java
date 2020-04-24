413212dc66a2d61caf567f480c7fc6269e49f27e
emzoo16 <43309131+emzoo16@users.noreply.github.com>
2020-04-24 17:00:47 +1200

    Allow dynamic users to request feeds (#168)
    * use auth token in header for webclient
    
    * fix code style
    
    * comment out changes
    
    * fix style
    
    * remove credentials from properties file
    
    Co-authored-by: Babra Ajaz <48302817+baja156@users.noreply.github.com>

--- server/src/main/java/com/feeder/server/provider/twitter/TwitterFeedProvider.java
+++ server/src/main/java/com/feeder/server/provider/twitter/TwitterFeedProvider.java
@@ -4,6 +4,7 @@ import com.feeder.server.model.TwitterData;
 import com.feeder.server.provider.FeedProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -20,12 +21,18 @@ import twitter4j.TwitterException;
 @Service
 public class TwitterFeedProvider implements FeedProvider<TwitterData> {
 
+  private static final String TWITTER_API_BASE_URL = "https://api.twitter.com/1.1";
   @Autowired private Twitter twitter;
 
+  private WebClient.Builder webClientBuilder;
   private ResponseList<Status> lastResponse;
 
   @Override
   public Flux<TwitterData> getFeed() {
+    // The endpoint that feeder uses to get twitter posts
+    // https://api.twitter.com/1.1/statuses/home_timeline
+    //  (from http://twitter4j.org/javadoc/twitter4j/api/TimelinesResources.html#getHomeTimeline--)
+
     // Gets the twitter posts from the user's home feed
     return Mono.fromCallable(() -> twitter.getHomeTimeline())
         .subscribeOn(Schedulers.elastic())
@@ -56,7 +63,37 @@ public class TwitterFeedProvider implements FeedProvider<TwitterData> {
                   .tweetPostDate(status.getCreatedAt())
                   .build();
             });
+
+    // NEW CHANGES
+    //      WebClient webClient = getWebClientBuilder().build();
+    //      return webClient
+    //              .get()
+    //              .uri(TWITTER_API_BASE_URL + "/statuses/home_timeline")
+    //              .headers(headers -> headers.setBearerAuth(""))
+    //              .exchange()
+    //              .map(response ->{
+    //                  TwitterData.Builder twitterBuilder = TwitterData.newBuilder();
+    //                  return twitterBuilder
+    //                      .tweet(response.)
+    //                      .profileUsername(status.getUser().getName())
+    //                      .profileImageURI(status.getUser().get400x400ProfileImageURL())
+    //                      .tweetPostDate(status.getCreatedAt())
+    //                      .build();
+    //              });
+
+    // tweet:text, profileUsername:user.name, profileimageuri:
+
   }
+
+  //    private WebClient.Builder getWebClientBuilder() {
+  //        if (this.webClientBuilder == null) {
+  //            this.webClientBuilder =
+  //                    WebClient.builder()
+  //                            .baseUrl(TWITTER_API_BASE_URL)
+  //                            .defaultHeader(HttpHeaders.AUTHORIZATION);
+  //        }
+  //        return this.webClientBuilder;
+  //    }
 
   ResponseList<Status> getLastResponse() {
     return lastResponse;
