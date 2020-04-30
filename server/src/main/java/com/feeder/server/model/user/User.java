8c8ce62d0406aee86fd5cf225864f15f1c6c8c71
Emily Zou <emilyzou98@gmail.com>
2020-04-30 20:24:07 +1200

    remove credentials from properties file

--- server/src/main/java/com/feeder/server/model/user/User.java
+++ server/src/main/java/com/feeder/server/model/user/User.java
@@ -58,12 +58,12 @@ public class User {
 
   public void updateAccessToken(AccessToken accessToken) {
     String appType = accessToken.getApp();
-    if(getAccessTokenByApp(appType)!= null) {
-      getAccessTokenByApp(appType).setToken(accessToken.getToken());
+    if(getExistingAccessTokenByApp(appType)!= null) {
+      getExistingAccessTokenByApp(appType).setToken(accessToken.getToken());
     }
   }
 
-  public AccessToken getAccessTokenByApp(String appType){
+  public AccessToken getExistingAccessTokenByApp(String appType){
     for (AccessToken token : accessTokens) {
       if (token.getApp().equals(appType)) {
         return token;
